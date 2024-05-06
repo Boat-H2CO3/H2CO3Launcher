2be539745a45a0a556fe08e7ae5f3fc503693b7b
Shuiling <1668589925@qq.com>
2024-05-07 02:42:01 +0800

    Refactor: Simplify code with record and switch expressions

--- H2CO3Core/src/main/java/org/koishi/launcher/h2co3/core/download/LibraryAnalyzer.java
+++ H2CO3Core/src/main/java/org/koishi/launcher/h2co3/core/download/LibraryAnalyzer.java
@@ -122,7 +122,7 @@ public final class LibraryAnalyzer implements Iterable<LibraryAnalyzer.LibraryMa
 
     public boolean hasModLoader() {
         return libraries.keySet().stream().map(LibraryType::fromPatchId)
-                .filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull).filter(Objects::nonNull)
+                .filter(Objects::nonNull)
                 .anyMatch(LibraryType::isModLoader);
     }
 
